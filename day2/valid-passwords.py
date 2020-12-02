0fb7acf5fafd2b4e76f95a9e2f06c0824ca7a9c1
Danny <danny@blend.com>
2020-12-01 22:14:30 -0800

    day 2 part 2

--- day2/valid-passwords.py
+++ day2/valid-passwords.py
@@ -1,14 +1,29 @@
 with open('passwords.txt', 'r') as f:
     content = f.readlines()
 
+
+#Part 1
+# validCount = 0
+# for line in content:
+#     splitLine = line.replace('-', ' ').split()
+#     match = splitLine[2][0]
+#     i = 0
+#     for letter in splitLine[3]:
+#         if letter == match: i+=1
+#     if i >= int(splitLine[0]) and i <= int(splitLine[1]):
+#         validCount += 1
+
+# print(validCount)
+
 validCount = 0
 for line in content:
     splitLine = line.replace('-', ' ').split()
     match = splitLine[2][0]
     i = 0
-    for letter in splitLine[3]:
-        if letter == match: i+=1
-    if i >= int(splitLine[0]) and i <= int(splitLine[1]):
+    pw = splitLine[3]
+    if pw[int(splitLine[0]) - 1] == match: i+=1
+    if pw[int(splitLine[1]) - 1] == match: i+=1
+    if i == 1:
         validCount += 1
 
 print(validCount)
