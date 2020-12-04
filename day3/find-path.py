83944e8e165b43bdf2643c1e1078e23a167212e9
Danny <danny@blend.com>
2020-12-03 21:33:31 -0800

    day3 part 2

--- day3/find-path.py
+++ day3/find-path.py
@@ -3,15 +3,43 @@ with open('map.txt', 'r') as f:
 content = [x.strip() for x in content]
 
 #Part 1
+# height = len(content)
+# width = len(content[0])
+# pos = [0, 0]
+# trees = 0
+
+# while pos[1] < height - 1:
+#     pos[0] += 3
+#     pos[1] += 1
+#     if content[pos[1]][pos[0] % width] == '#':
+#         trees += 1
+
+# print(trees)
+
+#Part 2
 height = len(content)
 width = len(content[0])
-pos = [0, 0]
-trees = 0
+trees = []
 
-while pos[1] < height - 1:
-    pos[0] += 3
-    pos[1] += 1
-    if content[pos[1]][pos[0] % width] == '#':
-        trees += 1
+slopes = [
+    (1,1),
+    (3,1),
+    (5,1),
+    (7,1),
+    (1,2)
+]
 
-print(trees)
+for slope in slopes:
+    trees.append(0)
+    pos = [0, 0]
+
+    while pos[1] < height - 1:
+        pos[0] += slope[0]
+        pos[1] += slope[1]
+        if content[pos[1]][pos[0] % width] == '#':
+            trees[-1] += 1
+
+treemultiple = 1
+for treeCount in trees:
+    treemultiple *= treeCount
+print(treemultiple)
