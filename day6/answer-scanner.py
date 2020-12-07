389e7a21a3b0502348031448dae621126acf4df5
Danny <danny@blend.com>
2020-12-07 12:12:59 -0800

    day 6 part 2

--- day6/answer-scanner.py
+++ day6/answer-scanner.py
@@ -3,17 +3,37 @@ with open('answers.txt', 'r') as f:
 content = [x.strip() for x in content]
 
 #Part 1
+# groupID = 0
+# groups = {}
+
+# for row in content:
+#     if row == '': groupID +=1
+#     else:
+#         for answer in row:
+#             if groupID in groups:
+#                 groups[groupID].add(answer)
+#             else:
+#                 groups[groupID] = {answer}
+
+# yesCount = 0
+# for group in groups.values():
+#     yesCount += len(group)
+# print(yesCount)
+
+# Part 2
 groupID = 0
 groups = {}
 
 for row in content:
     if row == '': groupID +=1
+    elif groupID in groups:
+        for answer in groups[groupID].copy():
+            if answer not in row:
+                groups[groupID].remove(answer)
     else:
+        groups[groupID] = set()
         for answer in row:
-            if groupID in groups:
-                groups[groupID].add(answer)
-            else:
-                groups[groupID] = {answer}
+            groups[groupID].add(answer)
 
 yesCount = 0
 for group in groups.values():
